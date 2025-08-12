dfce1b8f12fffcdce0965fadde69f279ebf67fee
Daniel Esquinazi <esquinazi.dani@gmail.com>
2025-08-12 12:55:26 -0300

    Add analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { PhraseView } from "@/phrase";
 import { Toaster } from "@/components/ui/sonner";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <>
+      <Analytics />
       <Toaster />
       <main className="h-[100dvh] w-full md:max-w-2/3 flex flex-col items-center p-2 mx-auto">
         <PhraseView />
